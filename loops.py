bbc06edd07aa864196fc2c53121167ff86e352a9
Abhijit Kamat <avk41+blah@scarletmail.rutgers.edu>
2018-06-02 15:49:11 -0400

    Print out currently testing value first

--- loops.py
+++ loops.py
@@ -1,7 +1,7 @@
 student_names = ["AJ", "Andrew", "Mehul", "Jean Carlos", "George", "Josiah", "Gurpreet", "Eric", "Jack", "Sergio", "Aaron", "Samir", "Mariel"]
 
 for name in student_names:
+    print(f"Currently Testing: {name}")
     if name == "AJ":
         print(f"Found him! {name}")
         break;
-    print(f"Currently Testing: {name}")
