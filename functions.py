9ed8300293193c3190bcc431306a0958984cea60
Abhijit Kamat <avk41+blah@scarletmail.rutgers.edu>
2018-06-02 17:50:17 -0400

    additional functions for making a student manager application

--- functions.py
+++ functions.py
@@ -1,3 +1,23 @@
+students = []
+
+
+def get_students_titlecase():
+    students_titlecase = []
+    for student in students:
+        students_titlecase = student.title()
+    return students_titlecase
+
+
+def print_students_titlecase():
+    students_titlecase = get_students_titlecase()
+    print(students_titlecase)
+
+
+def add_student(name, student_id=1234):
+    student = {"name": name, "student_id": student_id}
+    students.append(student)
+
+
 def var_args(name, **kwargs):
     print(name)
     print(kwargs["description"], kwargs["feedback"])
