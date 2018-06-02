dc0a399c628af55e2c3470611020837e0313a5ed
Abhijit Kamat <avk41+blah@scarletmail.rutgers.edu>
2018-06-02 15:47:56 -0400

    looping example

---
+++ loops.py
@@ -0,0 +1,6 @@
+student_names = ["AJ", "Andrew", "Mehul", "Jean Carlos", "George", "Josiah", "Gurpreet", "Eric", "Jack", "Sergio", "Aaron", "Samir", "Mariel"]
+
+for name in student_names:
+    if name == "AJ":
+        print(f"Found him! {name}")
+    print(f"Currently Testing: {name}")
