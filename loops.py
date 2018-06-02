3dfa01f7a0c25b4bbd6587cdacab36680ef5a18f
Abhijit Kamat <avk41+blah@scarletmail.rutgers.edu>
2018-06-02 15:48:39 -0400

    break after finding name

--- loops.py
+++ loops.py
@@ -3,4 +3,5 @@ student_names = ["AJ", "Andrew", "Mehul", "Jean Carlos", "George", "Josiah", "Gu
 for name in student_names:
     if name == "AJ":
         print(f"Found him! {name}")
+        break;
     print(f"Currently Testing: {name}")
