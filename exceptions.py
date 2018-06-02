88ceef2add885ac8843b3fcc0e1d976e7e13eaff
Abhijit Kamat <avk41+blah@scarletmail.rutgers.edu>
2018-06-02 17:10:38 -0400

    TypeError

--- exceptions.py
+++ exceptions.py
@@ -1,10 +1,8 @@
-student={
-    "name": "Sergio",
-    "employee_id": 123488,
-    "feedback": None
-}
+student= {"name": "Sergio", "employee_id": 123488, "feedback": None, "lastname": "Guerra"}
 
 try:
     lastname = student["lastname"]
+    numbered_lastname = 3+lastname
+
 except KeyError:
     print("Error finding lastname")
